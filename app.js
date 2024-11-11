6e7c8e45bd1a7477699c7fc58c65175078b63b55
Arccobaleno <162308538+Arccobaleno@users.noreply.github.com>
2024-11-11 18:48:30 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+let currentPage = 1;
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchUsers(currentPage);
+
+  document.getElementById("next").addEventListener("click", () => {
+    currentPage++;
+    fetchUsers(currentPage);
+  });
+
+  document.getElementById("prev").addEventListener("click", () => {
+    currentPage--;
+    fetchUsers(currentPage);
+  });
+});
+
+function fetchUsers(page) {
+  fetch(`https://randomuser.me/api/?page=${page}&results=5`)
+    .then(response => response.json())
+    .then(data => {
+      displayUsers(data.results);
+      toggleButtons(page);
+    })
+    .catch(error => console.log('Error:', error));
+}
+
+function displayUsers(users) {
+  const tableBody = document.getElementById('userData');
+  tableBody.innerHTML = '';
+  users.forEach(user => {
+    const row = `<tr>
+      <td><img src="${user.picture.thumbnail}" alt="User Photo"></td>
+      <td>${user.name.first} ${user.name.last}</td>
+      <td>${user.email}</td>
+      <td>${user.phone}</td>
+      <td>${user.location.city}, ${user.location.country}</td>
+    </tr>`;
+    tableBody.innerHTML += row;
+  });
+}
+
+function toggleButtons(page) {
+  document.getElementById("prev").disabled = page === 1;
+}
